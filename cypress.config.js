9d4034b388e659949f6dacb8f175f2f89df47e9f
Jiron <59139789+Jiron@users.noreply.github.com>
2025-01-19 07:08:35 +0100

    Revert "fix: some formattings & work on cypress"
    This reverts commit a6427fc8b9c3c31c9357902677154a948218f846.

--- cypress.config.js
+++ cypress.config.js
@@ -4,9 +4,7 @@ export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:5173",
     setupNodeEvents(on, config) {
-      require('@cypress/code-coverage/task')(on, config)
-
-      return config
+      // implement node event listeners here
     },
   },
 });
