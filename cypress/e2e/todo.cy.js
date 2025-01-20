0abfe86d3a7cf1f799065eeccaf9a3cceb43ac04
Yammy33 <luke.meier1@gmail.com>
2025-01-20 08:14:27 +0100

    things

--- cypress/e2e/todo.cy.js
+++ cypress/e2e/todo.cy.js
@@ -1,47 +1,81 @@
 describe("todo tests", () => {
+  const uniqueSeed = Date.now().toString();
+  const getUniqueId = () => Cypress._.uniqueId(uniqueSeed);
+  let category = "";
+
   beforeEach(() => {
     cy.visit("/");
+    category = getUniqueId();
+    cy.get('input[placeholder="New category"]').type(`${category}`)
+    cy.contains("button", "Add").click()
   });
 
   it("should render the todo app", () => {
     cy.getDataTest("todo-header").should("exist");
   });
 
-  // it("should add a todo", () => {
-  //   cy.get('[data-set="todo-form"] ').should("exist");
-  //   cy.get('[data-set="todo-form"] input[name = "title"]').type(
-  //     "Watch a movie"
-  //   );
-  //   cy.get('[data-set = "add-todo-btn"]').should("exist").click();
-  //   cy.contains("Watch a movie").should("exist");
-  // });
+  it("add category", () => {
 
-  // it("should mark a todo as completed", () => {
-  //   cy.get('[data-set="todo-form"] input[name = "title"]').type(
-  //     "Watch a movie"
-  //   );
-  //   cy.get('[data-set = "add-todo-btn"]').should("exist").click();
-  //   cy.contains("Watch a movie").should("exist");
-  //   cy.get('[data-set="todo-list"] input[type ="checkbox"]')
-  //     .should("exist")
-  //     .should("not.be.checked");
-  //   // you can also use should have class completed to check if the todo is completed
-  //   cy.get('[data-set="todo-list"] input[type ="checkbox"]')
-  //     .should("exist")
-  //     .click()
-  //     .should("be.checked");
-  // });
+    const uniqueId = getUniqueId();
+    cy.contains("h1", "todos")
+    cy.get('input[placeholder="New category"]').type(`${uniqueId}`)
+    cy.contains("button", "Add").click()
 
-  // it("should delete a todo", () => {
-  //   cy.get('[data-set="todo-form"] input[name = "title"]').type(
-  //     "Watch a movie"
-  //   );
-  //   cy.get('[data-set = "add-todo-btn"]').should("exist").click();
-  //   cy.contains("Watch a movie").should("exist");
+    cy.get(`input[value="${uniqueId}"]`)
+  })
 
-  //   cy.get('[data-set="todo-list"] [data-set="delete-todo-btn"]')
-  //     .should("exist")
-  //     .click();
-  //   cy.contains("Watch a movie").should("not.exist");
-  // });
+  it("update category", () => {
+    const uniqueId = getUniqueId();
+    cy.contains("h1", "todos")
+    cy.get('input[placeholder="New category"]').type(`${uniqueId}`)
+    cy.contains("button", "Add").click()
+
+    cy.get(`input[value="${uniqueId}"]`).type(`ab`)
+
+    cy.get(`input[value="${uniqueId}b"]`)
+  })
+
+  it("add task", () => {
+    const uniqueId = getUniqueId();
+    cy.contains("h1", "todos")
+    cy.get('input[placeholder="Add todo..."]').type(`${uniqueId}`)
+    cy.get('select[name="category"]').select(category)
+    cy.get("button[class='input-submit']").click()
+
+    cy.contains("span", uniqueId)
+  })
+
+  it("sort by priority", () => {
+    const uniqueId = getUniqueId();
+    cy.contains("h1", "todos")
+    cy.get('input[placeholder="Add todo..."]').type(`${uniqueId} low`)
+    cy.get('select[name="category"]').select(category)
+    cy.get("button[class='input-submit']").click()
+
+    cy.get('input[placeholder="Add todo..."]').type(`${uniqueId} high`)
+    cy.get('select[name="category"]').select(category)
+    cy.get('select[name="priority"]').select("High")
+    cy.get("button[class='input-submit']").click()
+
+    cy.contains("button", "Sort by Priority").click();
+
+
+    cy.get("span").first().contains("HIGH");
+    cy.get("span").last().contains("LOW");
+  })
+
+  it("get Task by Category", () => {
+    const uniqueId = getUniqueId();
+    cy.contains("h1", "todos")
+    cy.get('input[placeholder="Add todo..."]').type(`${uniqueId}`)
+    cy.get('select[name="category"]').select(category)
+    cy.get("button[class='input-submit']").click()
+
+    cy.get("div[class='_modeButtons_17a5m_11']").within(() => {
+      cy.get('select').select(category);
+    })
+
+
+    cy.contains("span", uniqueId)
+  })
 });
